3cf03438a7e40a9d0f8f5b925a2285e5fd7d76d3
Roman Beniuk <executwbxrn@gmail.com>
2026-03-05 12:42:37 +0000

    ref: tests

--- lib/features/home/domain/repos/pokemon_repo.dart
+++ lib/features/home/domain/repos/pokemon_repo.dart
@@ -1,7 +1,5 @@
 import 'package:pokemon/shared/domain/entities/pokemon_entity.dart';
 
-import '/shared/data/models/pokemon.dart';
-
 abstract class PokemonRepo {
   Future<List<PokemonEntity>> getAllPokemons();
   Future<PokemonEntity?> getPokemonById(int id);
